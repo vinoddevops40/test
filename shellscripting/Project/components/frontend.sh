a95608bb6778fbdc4c90b85767111621d504d2e2
vinoddevops40 <vinoddevops40@gmail.com>
2021-05-10 16:58:33 -0500

    commit

--- shellscripting/Project/components/frontend.sh
+++ shellscripting/Project/components/frontend.sh
@@ -3,7 +3,7 @@
 echo Install Frontend Component
 
 USER_ID=$(id -u)
-if [ ${USER_ID}) -ne 0 ]; then
+if [ "${USER_ID}" -ne 0 ]; then
   echo -e "\e[1;31mYou should be a root user to execute the command\e[0m"
   exit
 fi
