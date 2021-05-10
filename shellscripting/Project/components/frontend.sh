da4a2efb6a96ad1a299189851139447b4f10380e
vinoddevops40 <vinoddevops40@gmail.com>
2021-05-10 18:25:53 -0500

    commit

--- shellscripting/Project/components/frontend.sh
+++ shellscripting/Project/components/frontend.sh
@@ -22,7 +22,19 @@ fi
 echo "---------------------------------------------------------------"
 systemctl enable nginx
 systemctl start nginx
+echo "---------------------------------------------------------------"
+echo "\e[1;35m [INFO] Download Frontend components \e[0m"
+echo "---------------------------------------------------------------"
 curl -s -L -o /tmp/frontend.zip "https://github.com/roboshop-devops-project/frontend/archive/main.zip"
+if [ $? -ne 0 ]; then
+  echo "---------------------------------------------------------------"
+  echo -e "\e[1;31m [ERROR] Download Frontend components is failure \e[0m"
+  exit 2
+else
+  echo "---------------------------------------------------------------"
+  echo -e "\e[1;32m [ERROR] Download Frontend components is successful \e[0m"
+fi
+echo "---------------------------------------------------------------"
 cd /usr/share/nginx/html
 rm -rf *
 unzip /tmp/frontend.zip
