14ea17afe94f447450b69083273b009570aad9f7
vinoddevops40 <vinoddevops40@gmail.com>
2021-05-10 10:43:06 -0500

    commit

--- shellscripting/print.sh
+++ shellscripting/print.sh
@@ -3,7 +3,7 @@ echo Hello World
 echo How are you
 ls
 cat /etc/password
-echo -e "Hello World/nHow are you"
-echo -e "Hello World/n/nHow are you"
+echo -e "Hello World\nHow are you"
+echo -e "Hello World\n\nHow are you"
 
 
