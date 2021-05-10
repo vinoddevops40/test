f6ddf5c7c03c2713af1da95104eb1c4c71f1ec09
vinoddevops40 <vinoddevops40@gmail.com>
2021-05-10 17:16:24 -0500

    commit

--- shellscripting/Project/components/common.sh
+++ shellscripting/Project/components/common.sh
@@ -4,5 +4,5 @@ Echo "check user to see if component executed using root user"
 USER_ID=$(id -u)
 if [ "${USER_ID}" -ne 0 ]; then
   echo -e "\e[1;31mCheck Failed - You should be a root user to execute the command\e[0m"
-  exit
+  exit 1
 fi
--- shellscripting/Project/components/frontend.sh
+++ shellscripting/Project/components/frontend.sh
@@ -2,10 +2,10 @@
 #installing Front end component for Roboshop Project
 echo Install Frontend Component
 
-<<USER_ID=$(id -u)
-if [ "${USER_ID}" -ne 0 ]; then
-  echo -e "\e[1;31mYou should be a root user to execute the command\e[0m"
-  exit
-fi>>
+#USER_ID=$(id -u)
+#if [ "${USER_ID}" -ne 0 ]; then
+#  echo -e "\e[1;31mYou should be a root user to execute the command\e[0m"
+#  exit
+#fi
 
 yum install nginx
