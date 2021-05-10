f4972a01330a5c7fe9e54ee811f2ddc276bdaaa0
vinoddevops40 <vinoddevops40@gmail.com>
2021-05-10 16:51:20 -0500

    commit

--- shellscripting/Project/components/frontend.sh
+++ shellscripting/Project/components/frontend.sh
@@ -2,4 +2,9 @@
 #installing Front end component for Roboshop Project
 echo Install Frontend Component
 
+USER_ID=$(id -u)
+if [ "${USER_ID})" -ne 0]; then
+  echo -e "\e[1;31mYou should be a root user to execute the command\e[0m"
+  exit
+fi
 yum install nginx
