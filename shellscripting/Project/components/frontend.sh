76bd63b1b93f49a4388f33a7417ef162ca5ffa63
vinoddevops40 <vinoddevops40@gmail.com>
2021-05-10 18:06:16 -0500

    commit

--- shellscripting/Project/components/frontend.sh
+++ shellscripting/Project/components/frontend.sh
@@ -7,9 +7,19 @@ echo Install Frontend Component
 #  echo -e "\e[1;31mYou should be a root user to execute the command\e[0m"
 #  exit
 #fi
-
+echo "---------------------------------------------------------------"
+echo "\e[1;35m [INFO] Installing NGINX \e[0m"
+echo "---------------------------------------------------------------"
 yum install nginx -y
-
+if [ $? -ne 0 ]; then
+  echo "---------------------------------------------------------------"
+  echo -e "\e[1;31m [ERROR] Nginx installation is failure \e[0m"
+  exit 2
+else
+  echo "---------------------------------------------------------------"
+  echo -e "\e[1;32m [ERROR] Nginx installation is successful \e[0m"
+fi
+echo "---------------------------------------------------------------"
 systemctl enable nginx
 systemctl start nginx
 curl -s -L -o /tmp/frontend.zip "https://github.com/roboshop-devops-project/frontend/archive/main.zip"
