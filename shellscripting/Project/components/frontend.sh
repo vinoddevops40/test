15610eb334757b574d7cd25f78a527b17f9b06fc
vinoddevops40 <vinoddevops40@gmail.com>
2021-05-10 17:21:10 -0500

    commit

--- shellscripting/Project/components/frontend.sh
+++ shellscripting/Project/components/frontend.sh
@@ -9,3 +9,15 @@ echo Install Frontend Component
 #fi
 
 yum install nginx
+
+systemctl enable nginx
+systemctl start nginx
+curl -s -L -o /tmp/frontend.zip "https://github.com/roboshop-devops-project/frontend/archive/main.zip"
+cd /usr/share/nginx/html
+rm -rf *
+unzip /tmp/frontend.zip
+mv frontend-main/* .
+mv static/* .
+rm -rf frontend-master README.md
+mv localhost.conf /etc/nginx/default.d/roboshop.conf
+systemctl restart nginx
