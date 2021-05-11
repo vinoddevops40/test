fa7254cc230a4cf8f004a4ba987b9f4e97e0721f
vinoddevops40 <vinoddevops40@gmail.com>
2021-05-10 21:54:19 -0500

    commit

--- shellscripting/Project/components/frontend.sh
+++ shellscripting/Project/components/frontend.sh
@@ -14,27 +14,26 @@ PRINT "NGINX Install"
 yum install nginx -y
 STAT $? "NGINX Install"
 
-systemctl enable nginx
-systemctl start nginx
-
-echo "---------------------------------------------------------------"
-echo -e "\e[1;35m [INFO] Download Frontend components \e[0m"
-echo "---------------------------------------------------------------"
+PRINT "Download Frontend components"
 curl -s -L -o /tmp/frontend.zip "https://github.com/roboshop-devops-project/frontend/archive/main.zip"
-if [ $? -ne 0 ]; then
-  echo "---------------------------------------------------------------"
-  echo -e "\e[1;31m [ERROR] Download Frontend components is failure \e[0m"
-  exit 2
-else
-  echo "---------------------------------------------------------------"
-  echo -e "\e[1;32m [ERROR] Download Frontend components is successful \e[0m"
-fi
-echo "---------------------------------------------------------------"
+STAT $? "Download Frontend "
+
+PRINT "Extract frontend"
 cd /usr/share/nginx/html
 rm -rf *
 unzip /tmp/frontend.zip
+STAT $? "Extracting frontend"
+
 mv frontend-main/* .
 mv static/* .
 rm -rf frontend-master README.md
+
+
+PRINT "Update Nginx config"
 mv localhost.conf /etc/nginx/default.d/roboshop.conf
+STAT $? "Updating Nginx config"
+
+PRINT "Start Nginx Service"
+systemctl enable nginx
 systemctl restart nginx
+STAT $? "Start Nginx Service"
