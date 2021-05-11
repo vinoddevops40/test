d2131a33d7e36047bf4e2ea484a8832101be58c0
vinoddevops40 <vinoddevops40@gmail.com>
2021-05-11 18:31:28 -0500

    commit

---
+++ shellscripting/Project/components/mongodb.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+source components/common.sh
+echo Install mongodb Component
+
+PRINT "mongodb Install"
+echo '[mongodb-org-4.2]
+name=MongoDB Repository
+baseurl=https://repo.mongodb.org/yum/redhat/$releasever/mongodb-org/4.2/x86_64/
+gpgcheck=1
+enabled=1
+gpgkey=https://www.mongodb.org/static/pgp/server-4.2.asc' >/etc/yum.repos.d/mongodb.repo
+STAT $? "installing mongodb"
+
