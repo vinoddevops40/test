e46e2181f911d0368311ae44841b239bcff5accd
vinoddevops40 <vinoddevops40@gmail.com>
2021-05-11 11:21:28 -0500

    commit

--- shellscripting/Project/instance-create/create.sh
+++ shellscripting/Project/instance-create/create.sh
@@ -2,8 +2,15 @@
 
 LID=lt-0da1fb56f241b319b
 LVER=1
+COMPONENT=$1
 
-aws ec2 run-instances  --launch-template LaunchTemplateId=${LID},Version=${LVER} | jq
+if [ -z "${COMPONENT}" ];
+  echo "Component Name Input Needed"
+  exit 1
+fi
+
+aws ec2 run-instances  --launch-template LaunchTemplateId=${LID},Version=${LVER} --tag-specifications
+          "ResourceType=instance,Tags=[{Key=Name,Value=${COMPONENT}]" | jq
 
 
 
