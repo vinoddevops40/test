4e1ad876832c9ac78b4678a1890ef66ac0ac488b
vinoddevops40 <vinoddevops40@gmail.com>
2021-05-12 09:58:47 -0500

    commit

--- shellscripting/Project/instance-create/create.sh
+++ shellscripting/Project/instance-create/create.sh
@@ -9,7 +9,16 @@ if [ -z "${COMPONENT}" ]; then
   exit 1
 fi
 
-aws ec2 run-instances  --launch-template LaunchTemplateId=${LID},Version=${LVER}  --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=${COMPONENT}}]" | jq
+
+INSTANCE_EXIST=aws ec2 describe-instances --filters Name=tag:Name,Values=${COMPONENT} | jq .Reservations[]
+
+if [ -z $(INSTANCE_EXIST) ]; then
+
+    aws ec2 run-instances  --launch-template LaunchTemplateId=${LID},Version=${LVER}  --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=${COMPONENT}}]" | jq
+else
+  echo "Instance ${COMPONENT} already exist"
+
+fi
 
 
 
