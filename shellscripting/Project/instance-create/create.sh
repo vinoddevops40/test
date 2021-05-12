e526dcb19d49166321b2002650aa0f688754a311
vinoddevops40 <vinoddevops40@gmail.com>
2021-05-12 11:03:27 -0500

    commit

--- shellscripting/Project/instance-create/create.sh
+++ shellscripting/Project/instance-create/create.sh
@@ -10,8 +10,8 @@ if [ -z "${COMPONENT}" ]; then
 fi
 
 
-INSTANCE_EXIST=aws ec2 describe-instances --filters Name=tag:Name,Values=${COMPONENT} | jq .Reservations[]
-STATE=aws ec2 describe-instances --filters Name=tag:Name,Values=user | jq .Reservations[].Instances[].State.Name | xargs
+INSTANCE_EXIST=$(aws ec2 describe-instances --filters Name=tag:Name,Values=${COMPONENT} | jq .Reservations[])
+STATE=$(aws ec2 describe-instances --filters Name=tag:Name,Values=user | jq .Reservations[].Instances[].State.Name | xargs)
 
 if [ -z $(INSTANCE_EXIST) -o "$STATE" == "terminated" ]; then
 
@@ -21,7 +21,7 @@ else
 
 fi
 
-IPADDRESS=aws ec2 describe-instances --filters Name=tag:Name,Values=user | jq .Reservations[].Instances[].PrivateIpAddress | grep -v null | xargs
+IPADDRESS=$(aws ec2 describe-instances --filters Name=tag:Name,Values=user | jq .Reservations[].Instances[].PrivateIpAddress | grep -v null | xargs)
 sed -e "s/COMPONENT/${COMPONENT}/" -e "s/IPADDRESS/${IPADDRESS}/" record.json >/tmp/record.json
 aws route53 change-resource-record-sets --hosted-zone-id Z0119149I8S0IAPXEMO8 --change-batch file:///tmp/record.json
 
