78d1063ac1b1f131232625f87385e931c8148efd
vinoddevops40 <vinoddevops40@gmail.com>
2021-05-11 19:19:46 -0500

    commit

--- shellscripting/Project/components/cart.sh
+++ shellscripting/Project/components/cart.sh
@@ -1,3 +1,34 @@
 #!/bin/bash
 
 echo "installing cart through shell script"
+
+source components/common.sh
+echo Install cart Component
+
+PRINT "Nodejs Install"
+yum install nodejs make gcc-c++ -y
+STAT $? "Nodejs Install"
+
+PRINT "Add Application user for Roboshop"
+useradd roboshop
+STAT $? "Add Application user for Roboshop"
+
+PRINT "Download cart component"
+curl -s -L -o /tmp/cart.zip "https://github.com/roboshop-devops-project/cart/archive/main.zip"
+STAT $? "Download cart component"
+
+PRINT "Extract cart component"
+cd /home/roboshop
+unzip /tmp/cart.zip
+mv cart-main cart
+STAT $? "Extract cart component"
+
+PRINT "NPM Install"
+cd /home/roboshop/cart
+npm install
+STAT $? "NPM Install"
+
+#mv /home/roboshop/catalogue/systemd.service /etc/systemd/system/catalogue.service
+#systemctl daemon-reload
+#systemctl start catalogue
+#systemctl enable catalogue
--- shellscripting/Project/components/catalogue.sh
+++ shellscripting/Project/components/catalogue.sh
@@ -1,2 +1,33 @@
 #!/bin/bash
 echo "installing catalogue through shell script"
+
+source components/common.sh
+echo Install catalogue Component
+
+PRINT "Nodejs Install"
+yum install nodejs make gcc-c++ -y
+STAT $? "Nodejs Install"
+
+PRINT "Add Application user for Roboshop"
+useradd roboshop
+STAT $? "Add Application user for Roboshop"
+
+PRINT "Download Catalogue component"
+curl -s -L -o /tmp/catalogue.zip "https://github.com/roboshop-devops-project/catalogue/archive/main.zip"
+STAT $? "Download Catalogue component"
+
+PRINT "Extract Catalogue component"
+cd /home/roboshop
+unzip /tmp/catalogue.zip
+mv catalogue-main catalogue
+STAT $? "Extract Catalogue component"
+
+PRINT "NPM Install"
+cd /home/roboshop/catalogue
+npm install
+STAT $? "NPM Install"
+
+#mv /home/roboshop/catalogue/systemd.service /etc/systemd/system/catalogue.service
+#systemctl daemon-reload
+#systemctl start catalogue
+#systemctl enable catalogue
