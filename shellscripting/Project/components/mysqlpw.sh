6cd1f5072103bd4fcd7eb4f37d90efe828cac9af
vinoddevops40 <vinoddevops40@gmail.com>
2021-05-13 17:29:35 -0500

    commit

--- shellscripting/Project/components/mysqlpw.sh
+++ shellscripting/Project/components/mysqlpw.sh
@@ -4,10 +4,10 @@ echo show databases | mysql -uroot -pRoboshop@123
 if [ $? -ne 0 ]; then
 DEFAULT_PASSWORD=$(grep 'temporary password' /var/log/mysqld.log | awk '{print $NF}')
 echo "${DEFAULT_PASSWORD}"
-echo "ALTER USER 'root'@'localhost' IDENTIFIED BY 'RoboShop@123';
-      uninstall plugin validate_password;" >/tmp/sql
-
-mysql --connect-expired-password -u root -p"${DEFAULT_PASSWORD}" </tmp/sql
+#echo "ALTER USER 'root'@'localhost' IDENTIFIED BY 'RoboShop@123';
+#      uninstall plugin validate_password;" >/tmp/sql
+#
+#mysql --connect-expired-password -u root -p"${DEFAULT_PASSWORD}" </tmp/sql
 fi
 
 
