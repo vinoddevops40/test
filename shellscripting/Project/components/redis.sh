5db11d7990ac18f360fd8a75a67c4002d0ca853b
vinoddevops40 <vinoddevops40@gmail.com>
2021-05-13 11:12:44 -0500

    commit

--- shellscripting/Project/components/redis.sh
+++ shellscripting/Project/components/redis.sh
@@ -4,3 +4,17 @@ source components/common.sh
 echo Install catalogue Component
 COMPONENT=redis
 OS_Prereqs
+
+
+
+# yum install epel-release yum-utils -y
+# yum install http://rpms.remirepo.net/enterprise/remi-release-7.rpm -y
+# yum-config-manager --enable remi
+# yum install redis -y
+
+
+Update the BindIP from 127.0.0.1 to 0.0.0.0 in config file /etc/redis.conf & /etc/redis/redis.conf
+
+
+# systemctl enable redis
+# systemctl start redis
