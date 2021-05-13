d0ca1a1a26638aca6b228c36b373f7aa75e0dc07
vinoddevops40 <vinoddevops40@gmail.com>
2021-05-13 16:51:52 -0500

    commit

--- shellscripting/Project/components/mysqlpw.sh
+++ shellscripting/Project/components/mysqlpw.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 DEFAULT_PASSWORD=$(grep 'temporary password' /var/log/mysqld.log | awk '{print $NF}')
 echo "${DEFAULT_PASSWORD}"
+echo "ALTER USER 'root'@'localhost' IDENTIFIED BY 'RoboShop@123'",
 echo "uninstall plugin validate_password;
 ALTER USER 'root'@'localhost' IDENTIFIED BY 'password';
 " >/tmp/sql
