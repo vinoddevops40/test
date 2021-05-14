17d8aed57d0f53c9898f255cc5e9ebca5cbb7eec
vinoddevops40 <vinoddevops40@gmail.com>
2021-05-13 23:05:58 -0500

    commit

--- shellscripting/Project/components/mysqlpw.sh
+++ shellscripting/Project/components/mysqlpw.sh
@@ -1,15 +1,21 @@
 #!/bin/bash
 
+  DEFAULT_PASSWORD=$(grep 'temporary password' /var/log/mysqld.log | awk '{print $NF}')
+  echo "${DEFAULT_PASSWORD}"
+  echo "uninstall plugin validate_password;
+  ALTER USER 'root'@'localhost' IDENTIFIED BY 'Password';" >/tmp/sql
+  mysql --connect-expired-password -u root -p"${DEFAULT_PASSWORD}" </tmp/sql
+  STAT $? "Changing mysql default PQ"
 
-echo show databases | mysql -uroot -pRoboshop@123
-if [ $? -ne 0 ]; then
-DEFAULT_PASSWORD=$(grep 'temporary password' /var/log/mysqld.log | awk '{print $NF}')
-echo "${DEFAULT_PASSWORD}"
-echo "ALTER USER 'root'@'localhost' IDENTIFIED BY 'RoboShop123';
-uninstall plugin validate_password;
-ALTER USER 'root'@'localhost' IDENTIFIED BY 'password';" >/tmp/sql
-
-mysql --connect-expired-password -u root -p"${DEFAULT_PASSWORD}" </tmp/sql
-fi
+#echo show databases | mysql -uroot -pRoboshop@123
+#if [ $? -ne 0 ]; then
+#DEFAULT_PASSWORD=$(grep 'temporary password' /var/log/mysqld.log | awk '{print $NF}')
+#echo "${DEFAULT_PASSWORD}"
+#echo "ALTER USER 'root'@'localhost' IDENTIFIED BY 'RoboShop123';
+#uninstall plugin validate_password;
+#ALTER USER 'root'@'localhost' IDENTIFIED BY 'password';" >/tmp/sql
+#
+#mysql --connect-expired-password -u root -p"${DEFAULT_PASSWORD}" </tmp/sql
+#fi
 
 
